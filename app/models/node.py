4e8353403c90eebf9c550e1986e03160954f5631
Felipe Garcia <luisfelipeglondo@gmail.com>
2025-10-26 23:10:52 -0500

    modification Node model

--- app/models/node.py
+++ app/models/node.py
@@ -1,14 +1,25 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, FrozenSet
 from enum import Enum
 
-class TypeNode(Enum):
+class NodeType(Enum):
     MIN = 0
     MAX = 1
 
 @dataclass
+class State():
+    pts_min: int
+    pts_max: int
+    pos_min: tuple[int]
+    pos_max: tuple[int]
+    destroyed_squares: set[tuple[int]]
+    special_squares: dict[tuple, int]
+
+@dataclass
 class Node():
-    type: TypeNode
-    depth: int
-    utility: float
+    type: NodeType
     parent: Optional['Node']
+    state: State
+    depth: int
+    utility: int
+    
